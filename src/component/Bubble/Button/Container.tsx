ddc1144e855db0b43dd5289a0876f37d4bd2950c
Radhouane BENBRAHIM <radhouanebenbrahim@gmail.com>
2023-11-29 11:57:36 +0100

    add hover effects

--- src/component/Bubble/Button/Container.tsx
+++ src/component/Bubble/Button/Container.tsx
@@ -10,6 +10,12 @@ const Container = styled("div")(() => {
     width: "60px",
     borderRadius: "50%",
     backgroundColor: "rgb(46, 71, 93);",
+    boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)", // Add this line for the shadow
+    cursor: "pointer",
+    transition: "all 0.3s ease-in-out",
+    ":hover": {
+      transform: "scale(1.1)",
+    },
   };
 });
 
