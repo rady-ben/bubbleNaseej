e2131088e202f5c8bd45b463aea0cec2b0c22a02
Radhouane BENBRAHIM <radhouanebenbrahim@gmail.com>
2023-11-29 10:59:48 +0100

    first commit

---
+++ src/component/Bubble/Bubble.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Bubble() {
+  return (
+    <div>
+      <p
+        style={{
+          color: "#000",
+        }}
+      >
+        radhouane benbrahim
+      </p>
+    </div>
+  );
+}
