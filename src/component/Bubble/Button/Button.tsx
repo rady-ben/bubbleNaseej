fe79eeb33da108e5eb9fa867fe5b08f06804cc4a
Radhouane BENBRAHIM <radhouanebenbrahim@gmail.com>
2023-11-29 15:10:24 +0100

    add animetion to icon swith transition

--- src/component/Bubble/Button/Button.tsx
+++ src/component/Bubble/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "./Container";
 import ChatIcon from "@mui/icons-material/Chat";
 import CloseIcon from "@mui/icons-material/Close";
@@ -9,11 +9,35 @@ type Props = {
 };
 
 export default function Button({ isOpened, onClick }: Props) {
-  const Icon = isOpened ? CloseIcon : ChatIcon;
+  const [chatIconOpacity, setChatIconOpacity] = useState(0);
+  const [closeIconOpacity, setCloseIconOpacity] = useState(1);
 
   return (
-    <Container onClick={onClick}>
-      <Icon sx={{ color: "white" }} />
-    </Container>
+    <div>
+      <Container onClick={onClick}>
+        <ChatIcon
+          sx={{
+            opacity: chatIconOpacity,
+            transition: "opacity 0.3s ease-in",
+            position: "absolute",
+          }}
+        />
+        <CloseIcon
+          sx={{
+            opacity: closeIconOpacity,
+            transition: "opacity 0.3s ease-in",
+            position: "absolute",
+          }}
+        />
+      </Container>
+      <button
+        onClick={() => {
+          setChatIconOpacity(!chatIconOpacity ? 1 : 0);
+          setCloseIconOpacity(!closeIconOpacity ? 1 : 0);
+        }}
+      >
+        tap
+      </button>
+    </div>
   );
 }
